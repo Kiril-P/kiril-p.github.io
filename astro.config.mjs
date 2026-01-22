88566a42416ca8e81ae00486f1ea4d80b2a710a5
Kiril Petrovski <kirilpetrovski2005@gmail.com>
2026-01-22 19:48:56 +0100

    Update site URL to custom domain kpetrovski.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kiril-p.github.io',
+  site: 'https://kpetrovski.me',
   vite: {
     plugins: [tailwindcss()]
   },
